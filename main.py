70f8b16773926e13544f15e85a405e5211523899
YutoMito <167249680+YutoMito@users.noreply.github.com>
2024-04-22 22:33:40 +0200

    Update main.py
    Funcion cuadrado repetida

--- main.py
+++ main.py
@@ -42,3 +42,17 @@ lista_salarios_comision = list(map(calculo_salario, lista_empleados))
 for empleado in lista_salarios_comision:
   print(empleado)
 
+#####################
+
+def cuadrado(x):
+  return x ** 2
+
+numeros = [1, 2, 3, 4, 5]
+
+# Usando map() para aplicar la función cuadrado a cada elemento de la lista numeros
+resultado = list(map(cuadrado, numeros))
+
+# Convertir el resultado a una lista para visualización
+#resultado_lista = list(resultado)
+
+print(resultado)  # Salida: [1, 4, 9, 16, 25]
